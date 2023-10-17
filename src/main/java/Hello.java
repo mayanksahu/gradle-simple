b0664a56128dcbbe19b6e63839e2565159be924b
Mayank Sahu <myksahu@gmail.com>
2023-10-17 15:12:27 +0530

    one more line

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
   public static String GREETING = "Hello world!";
   public static String BYE = "Bye world!";
   public static String TATA = "tata world!";
+  public static String SAYONARA = "sayonara world!";
+
   
   public static void main(String []args) {
     
