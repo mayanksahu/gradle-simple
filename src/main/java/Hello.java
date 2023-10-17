32511803fcbd5f54b24d153e1581617506439e63
Mayank Sahu <myksahu@gmail.com>
2023-10-17 15:19:01 +0530

    test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
   public static String BYE = "Bye world!";
   public static String TATA = "tata world!";
   public static String SAYONARA = "sayonara world!";
+  public static String TATA1 = "tata world!";
+  public static String SAYONARA1 = "sayonara world!";
 
   
   public static void main(String []args) {
