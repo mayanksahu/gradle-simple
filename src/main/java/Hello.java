dfeccc88c03e4a5f15b13512fb03bdbdb12fcff3
Mayank Sahu <myksahu@gmail.com>
2023-11-01 15:16:01 +0530

    message

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
   public static String TATA1 = "tata world!";
   @SuppressWarnings("java:S1444")
   public static String SAYONARA1 = "sayonara world!";
+  public static String TATA2 = "tata world 2!";
   
   public static void main(String []args) {
     
